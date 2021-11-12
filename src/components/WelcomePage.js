cb11b2da7a4368a9c3175b8e870adf2f90062e2e
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-11-12 01:13:33 +0100

    Create Quote and WelcomePage components

---
+++ src/components/WelcomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WelcomePage = () => (
+  <div>
+    <h2>Welcome to Math Magicians</h2>
+    <p>text</p>
+    <p>more text</p>
+  </div>
+);
+
+export default WelcomePage;
