65f8837ccff5a8130a322f5e06154d15d9cee314
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-11-12 01:14:30 +0100

    Add BrowserRouter and Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './Calculator';
 import Header from './Header';
+import WelcomePage from './WelcomePage';
+import Quote from './Quote';
 
 const App = () => (
-  <div>
-    <Header />
-    <div className="calc-container">
-      <Calculator />
+  <Router basename="/math-magicians">
+    <div>
+      <Header />
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={<WelcomePage />}
+        />
+        <Route
+          path="/Calculator"
+          element={<Calculator />}
+        />
+        <Route
+          exact
+          path="/Quote"
+          element={(
+            <div className="container">
+              <Quote />
+            </div>
+        )}
+        />
+      </Routes>
     </div>
-  </div>
+  </Router>
 );
 
 export default App;
